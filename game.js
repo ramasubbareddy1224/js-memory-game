cf6af944af483be0a268b57c64b44abef0e6e3a4
ramasubbareddy1224 <ramasubbareddy24@gmail.com>
2020-04-10 10:58:55 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,54 @@
+const cardMasterItems = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'A', 'B', 'C', 'D', 'E', 'F'
+]
+cardMasterItems.sort(() => 0.5 - Math.random());
+var selectedItems = [];
+console.log(cardMasterItems);
+function generateBoard() {
+    var counter = 0;
+    var divmainContainer = document.querySelector('#container');
+    for (let index = 0; index < 3; index++) {
+        var divflex = document.createElement('div');
+        divflex.setAttribute('class', 'flex-container');
+        for (let inner = 0; inner < 4; inner++, counter++) {
+            var divinner = document.createElement('div');
+            divinner.setAttribute('class', 'inner-div-bg');
+            divinner.setAttribute('data-val', cardMasterItems[counter]);
+            divinner.innerHTML = "?";
+            divinner.addEventListener('click', checkElementMatch);
+            divflex.appendChild(divinner);
+        }
+        divmainContainer.appendChild(divflex);
+
+    }
+}
+function checkElementMatch(event) {
+
+    const element = event.target;
+    if (element.innerHTML == "?") {
+        const elementVal = element.getAttribute('data-val');
+        element.innerHTML = elementVal;
+        var item = { ele: element, val: elementVal };
+        selectedItems.push(item);
+        if (selectedItems.length == 2) {
+            if (selectedItems[0].val != selectedItems[1].val) {
+                setTimeout(() => {
+                    element.innerHTML = "?";
+                    selectedItems[0].ele.innerHTML = "?";
+                    selectedItems = [];
+                }, 1000);
+            } else {
+                selectedItems = [];
+                const matchedElements = Array.from(document.querySelectorAll('.inner-div-bg')).reduce((acc, curr) => acc += curr.innerHTML != "?" ? 1 : 0, 0);
+                if (matchedElements == cardMasterItems.length) {
+                    setTimeout(()=>{
+                        window.alert('You Won!!');
+                        location.reload();
+                    },500);
+                }
+            }
+
+        }
+    }
+}
+generateBoard();
